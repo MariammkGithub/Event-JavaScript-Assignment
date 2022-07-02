89ebb9e81735da601999a09b07b649241c285789
MariammkGithub <102911841+MariammkGithub@users.noreply.github.com>
2022-07-02 18:17:48 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,29 @@
+function changeColor() {
+    document.getElementById('abc').style.background = 'red'
+    console.log(document.getElementById('abc'))
+}
+
+function onclickcolor() {
+document.getElementById('bcd').style.background = 'purple'
+    console.log(document.getElementById('bcd'))
+}
+
+function clickcolor() {
+    document.getElementById('aaa').style.background = 'orange'
+        console.log(document.getElementById('aaa'))
+    }
+    
+    function changebg() {
+        document.getElementById('ccc').style.background = 'green'
+            console.log(document.getElementById('ccc'))
+        }
+
+        function changebgcolor() {
+            document.getElementById('bbb').style.background = 'brown'
+                console.log(document.getElementById('bbb'))
+            }
+
+            function changecase() {
+                document.getElementById('bbb').style.background = 'blue'
+                    console.log(document.getElementById('username'))
+                }
